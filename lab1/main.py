500be93ac2a81906904304349af76c2629fffa03
Ilya <darkovsky2@gmail.com>
2019-09-11 12:11:31 +0300

    Added encode and  decode Cesar

--- lab1/main.py
+++ lab1/main.py
@@ -1,15 +1,15 @@
-letters = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
 path_allBook = "lab1/allBook.txt"
 path_partBook = "lab1/partBook.txt"
 
 #Частота букв
 def letters_freq(path_in, path_out):
-    lfreq = {letter: 0 for letter in letters}
+    lfreq = {letter: 0 for letter in alphabet}
 
     with open(path_in) as f:
         text = f.read()
         for letter in text:
-            if (letter.lower() in letters):
+            if (letter.lower() in alphabet):
                 lfreq[letter.lower()] += 1
 
     sort_lfreq = sorted(lfreq.items(), key=lambda key: key[1], reverse=True) 
@@ -19,7 +19,7 @@ def letters_freq(path_in, path_out):
     
 #Частота биграмм
 def bigrams_freq(path_in, path_out):
-    bifreq = {i + j: 0 for i in letters for j in letters}
+    bifreq = {i + j: 0 for i in alphabet for j in alphabet}
 
     with open(path_in) as f:
         text = f.read()
@@ -33,10 +33,46 @@ def bigrams_freq(path_in, path_out):
         for el in sort_bifreq:
             f.write(str(el) + "\n")
 
+#Зашифровка шифром Цезаря
+def cesar_encode(path_in, path_out, key):
+    all_text = ''
+    with open(path_in, 'r') as fin:
+        all_text = fin.read()
+    
+    with open(path_out, 'w') as fout:
+        for letter in all_text:
+            encode_letter = letter
+            if letter.lower() in alphabet:
+                if letter.islower():
+                    encode_letter = alphabet[(alphabet.find(letter.lower()) + key) % len(alphabet)]
+                else:
+                    encode_letter = alphabet[(alphabet.find(letter.lower()) + key) % len(alphabet)].upper()
+            fout.write(encode_letter)
+
+#Расшифровка шифра Цезаря
+def cesar_decode(path_in, path_out, key):
+    all_text = ''
+    with open(path_in, 'r') as fin:
+        all_text = fin.read()
+    
+    with open(path_out, 'w') as fout:
+        for letter in all_text:
+            encode_letter = letter
+            if letter.lower() in alphabet:
+                if letter.islower():
+                    encode_letter = alphabet[(alphabet.find(letter.lower()) - key) % len(alphabet)]
+                else:
+                    encode_letter = alphabet[(alphabet.find(letter.lower()) - key) % len(alphabet)].upper()
+            fout.write(encode_letter)
+
 #Частотый анализ по буквам и биграммам в исходном тексте всей книги
-letters_freq(path_allBook, "lab1/letters_freq_all.txt")
-bigrams_freq(path_allBook, "lab1/bigrams_freq_all.txt")
+#letters_freq(path_allBook, "lab1/letters_freq_all.txt")
+#bigrams_freq(path_allBook, "lab1/bigrams_freq_all.txt")
 
 #Частотный анализ по буквам и биграммам в части исходного текста книги
-letters_freq(path_partBook, "lab1/letters_freq_part.txt")
-bigrams_freq(path_partBook, "lab1/bigrams_freq_part.txt")
+#letters_freq(path_partBook, "lab1/letters_freq_part.txt")
+#bigrams_freq(path_partBook, "lab1/bigrams_freq_part.txt")
+
+#Зашифровка и расшифровка шифром Цезаря части исходного текста книги
+cesar_encode(path_partBook, "lab1/partBook_encode.txt", 5)
+cesar_decode("lab1/partBook_encode.txt", "lab1/partBook_decode.txt", 5)
