d289b80e0a0a803f0159d1dc9280ab3afc7fda40
Witness-senpai <darkovsky2@gmail.com>
2019-10-03 03:17:10 +0300

    Вторая лаба заработала

--- lab2/client.py
+++ lab2/client.py
@@ -15,20 +15,47 @@ if 'y' in key:
     a = random.randint(100, 500)
     g = random.randint(10, 20)
     p = simples[random.randint(0, len(simples) - 1)]
-    
-    #Отправка основных данных второму клиенту
-    sock.sendall((name + ': send values a, g, p, A: \n' + \
+    print(name + ": отправка a, g, p, A собеседнику: " + 
+        str(a) + "\n" + \
+        str(g) + "\n" + \
+        str(p) + "\n" + \
+        str((g ** a) % p))
+    #Отправка сгенерированных данных второму клиенту
+    sock.sendall((name + '\n' + \
                 str(a) + "\n" + \
                 str(g) + "\n" + \
                 str(p) + "\n" + \
                 str((g ** a) % p)).encode())
+    print(name + ": ожидаю значение B от собеседника...")
     
-    
-else:
-    a = random.randint(100, 500)
-    print(name + ": ожидаю получения a, g, p,")
+    #Получение от второго клиента числа B, сгенерированного на основе отправленных данных
+    B = int(sock.recv(1024).decode("utf-8"))
+    print("От собеседника пришло B = " + str(B))
 
-    #sock.sendall((name + ': жду данные').encode())
+    #Генерация ключа
+    K = B ** a % p
+    print("Ключ : " + str(K))
+
+else:
+    b = random.randint(100, 500)
+    print(name + ": ожидаю b, g, p, A...")
+    #Получение сгенерированных данных
+    msg = sock.recv(1024).decode("utf-8").split('\n')
+    a = msg[0]
+    g = msg[1]
+    p = msg[2]
+    A = msg[3]
+    print("От собеседника пришло: \na = " + str(a) + \
+        "\ng = " + str(g) + "\np = " + str(p) + "\nA = " + str(A))
+    #Формирование на их основе ещё одного числа и его отправка
+    B = (int(g) ** int(b)) % int(p)
+    print(name + ": генерация B = g ^ b mod p = " + str(B))
+    print(name + ": отправка В собеседнику")
+    sock.sendall(str(B).encode())
+    
+    #Генерация ключа
+    K = int(A) ** int(b) % int(p)
+    print("Ключ : " + str(K))
 
 input()
 
@@ -36,3 +63,6 @@ msg = sock.recv(1024)
 print(msg.decode('utf-8'))
 
 sock.close()
+
+def generate_key(A, b, p):
+    return A ** b % p
--- lab2/server.py
+++ lab2/server.py
@@ -11,13 +11,23 @@ while True:
 
     #Когда оба клиента подключены
     if len(clients) == 2:
-        #print('Got connection from ' + clients[0][1][0] + " : " + str(clients[0][1][1]))
+        #Получение данных от первого клиента
         msg = clients[0][0].recv(1024).decode("utf-8") 
         msg = msg.split('\n')
         a = msg[1]
         g = msg[2]
         p = msg[3]
         A = msg[4]
-        print(str(a) + " " + str(p) + " " + str(g) + " " + str(A))
+        print("a = " + str(a) + "\ng = " + str(g) + "\np = " + str(p) + "\nA = " + str(A))
+
+        #Отправка полученных данных второму клиенту
+        clients[1][0].send((str(a) + "\n" + str(g) + "\n" + str(p) + "\n" + str(A)).encode())
+
+        #Получение данных(число B) от второго клиента
+        B = clients[1][0].recv(1024).decode("utf-8")
+        print("B = " + B)
+        #Переправка первому клиенту
+        clients[0][0].send(B.encode())
+        
 
 clients[0][0].close()
