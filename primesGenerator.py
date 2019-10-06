e1fe50c339dc3b029dde4a27bee77d98f101c752
Ilya <darkovsky2@gmail.com>
2019-10-06 12:53:54 +0300

    Доделан генератор больших простых чисел

--- primesGenerator.py
+++ primesGenerator.py
@@ -1,4 +1,6 @@
 import random
+import time
+import math
 
 #Генарация простых чисел с помощью теста Рабина-Мюллера
 #В параметрах выбирается 
@@ -14,13 +16,14 @@ class primeGenerator():
             x += str(random.randint(0, 1))
 
         #Принудительное добавление '1' в первый и последний разряды
+        #Чтобы число точно было нечётное и без незначащих нулей в начале
         x = '1' + x[1:-1] + '1'
 
         return int(x, base=2)
 
     #ТестРабина-Мюллера
     def __RabinMillerTest(self):
-        #Вычисление b - наибольшее кол-во делений prime-1 на 2
+        #Вычисление b - наибольшее кол-во целосисленных делений prime-1 на 2
         b = 0
         temp_prime = self.prime - 1
         while (temp_prime > 0):
@@ -29,45 +32,46 @@ class primeGenerator():
                 break
             b += 1
         
-        #Вычисление m такое, что prime = 1 + 2^b * m
+        #Вычисление m такое, что prime - 1 = 2^b * m
         m = int( (self.prime - 1) / 2 ** b )
 
-        #Выбор случайного а, при условии а < prime
-        a = random.randint(1, 100)
+        #Количество раундов(шагов) берётся порядка log2(prime)
+        for step in range(len(str(self.prime))):
+            #Выбор случайного а из отрезка [2, prime-2]
+            a = random.randint(2, self.prime - 2)
 
-        j = 0
-        z = a ** m % self.prime
-        #Prime скорее простое число
-        if (z == 1 or z == self.prime - 1):
-            return self.prime
-
-        while(True):
-            #Prime точно не простое число    
-            if (j > 0 and z == 1):
-                return -1
-
-            j += 1
-            if (j < b and z < self.prime - 1):
-                z = z * z % self.prime
+            #Вычисляем a ^ m mod prime
+            z = pow(a, m, self.prime)
+            #Prime скорее простое число
+            if (z == 1 or z == self.prime - 1):
                 continue
+            
+            #Внутренний цикл выполняется b-1 раз
+            for i in range(b - 1):
+                z = z * z % self.prime
 
-            #Prime скорее всего простое число
-            if (z == self.prime - 1):
-                return self.prime
-            #Prime точно не простое число
-            if (j == b and z != self.prime - 1):
-                return -1
+                #Точно не простое число    
+                if (z == 1):
+                    return False
+
+                #Вернуться к внешнему циклу
+                if (z == self.prime - 1):      
+                    break
+            return False
+        return True
 
     #minBit - минимальная длина в битах
     #maxBit - максимальная длина в битах
-    def getPrime(self, minBit=30, maxBit=60):
+    def nextPrime(self, minBit=50, maxBit=100):
         #Пока не нашли простое число генерируем новое и проверяем его тестом
         while True:
-           # self.prime = self.__genRandNum(minBit, maxBit)
-            self.prime = 5998757
+            self.prime = self.__genRandNum(minBit, maxBit)
             if (self.__RabinMillerTest()):
                 return self.prime
 
+s = time.time()
+
 pg = primeGenerator()
-prime = pg.getPrime()
-print(prime)
+prime = pg.nextPrime()
+
+print(f'Prime = {prime}, time = {time.time() - s}')
